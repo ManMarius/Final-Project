350674ed1e00544af4a9c5b65fff103974c55457
ManMarius <105811195+ManMarius@users.noreply.github.com>
2022-09-12 20:23:28 +0300

    Add files via upload

---
+++ JavaScript/final_project.js
@@ -0,0 +1,15 @@
+const goToHome = () => {
+  document.getElementById("home").scrollIntoView({ behavior: "smooth" });
+};
+const goToAbout = () => {
+  document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+};
+const goToEducation = () => {
+  document.getElementById("education").scrollIntoView({ behavior: "smooth" });
+};
+const goToProjects = () => {
+  document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
+};
+const goToContact = () => {
+  document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+};
